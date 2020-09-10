f5a152cf97202b90327fe8ae0bdf97b98fad0066
dcemmerson <emmersod@oregonstate.edu>
2020-09-10 14:11:06 -0700

    Timer class started...

---
+++ app/src/main/java/com/lovelace_scd/timed/model/Timer.kt
@@ -0,0 +1,69 @@
+/// filename: Timer.kt
+/// description: CountDownTimer subclass which manages time to next dose of medidation. We must
+///     pass in an instance of Medication, and Timer provides interface for UI to interact with
+///     when determining time to notify user of next dose, mark dose as taken and decrement
+///     Medication.amountRemaining, reschedule next dose notification, etc.
+
+package com.lovelace_scd.timed.model
+
+import android.os.CountDownTimer
+
+
+const val millisecondsPerDay = 24 * 60 * 60 * 1000;
+const val millisecondsPerMinute = 60 * 1000;
+const val millisecondsPerSecond = 1000;
+
+class Timer(val medication: Medication) :
+        CountDownTimer(medication.dosesPerTimePeriod / medication.daysPerTimePeriod * millisecondsPerDay,
+                millisecondsPerSecond as Long) {
+
+
+    private var millisecondsToNextDose : Long;
+    // Keep millisecondsNextDoseAdjusted separate from millisecondsToNextDose to allow
+    //  user option to continue to adjust all future dose times. For example, if user selects from
+    //  UI "Delay next dose 2 hours", when user eventually marks dose as taken, we can easily allow
+    //  user to schedule all next doses for 2 hours later.
+    private var millisecondsNextDoseAdjusted : Long;
+    private var skipNextDose : Boolean = false;
+    private var nextDoseReady : Boolean = false;
+
+    init {
+        millisecondsToNextDose = medication.dosesPerTimePeriod / medication.daysPerTimePeriod * millisecondsPerDay;
+        millisecondsNextDoseAdjusted = 0;
+    }
+
+    fun getSecondsToNextDose(): Long {
+        return millisecondsToNextDose / millisecondsPerSecond;
+    }
+
+    fun skipNextDose() {
+        skipNextDose = true;
+    }
+
+    fun markTaken() {
+        if(!nextDoseReady) throw Error("Next dose not ready.");
+        nextDoseReady = false;
+        medication.takeMed();
+    }
+
+    fun adjustNextDoseTime(addTime: Long) {
+        millisecondsNextDoseAdjusted += addTime;
+        this.
+    }
+
+
+
+    override fun onTick(p0: Long) {
+        millisecondsToNextDose = p0;
+    }
+
+    override fun onFinish() {
+        if(!skipNextDose) {
+            nextDoseReady = true;
+        }
+
+        skipNextDose = false;
+        this.start();
+
+    }
+}
