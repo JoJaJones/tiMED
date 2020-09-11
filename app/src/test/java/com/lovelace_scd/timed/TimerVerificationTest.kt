f2b112ad297943cdc65b121a06d8387325041e73
dcemmerson <emmersod@oregonstate.edu>
2020-09-11 12:42:20 -0700

    Edit read/write json unit test for slight-lier okay-er unit test.

--- app/src/test/java/com/lovelace_scd/timed/TimerVerificationTest.kt
+++ app/src/test/java/com/lovelace_scd/timed/TimerVerificationTest.kt
@@ -2,21 +2,30 @@ package com.lovelace_scd.timed
 
 import com.lovelace_scd.timed.io.MedJsonReader
 import com.lovelace_scd.timed.model.Timer
+import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.io.File
 
 
 class TimerVerificationTest {
-    @Test
-    fun jsonReaderTest() {
-        val medJsonReader = MedJsonReader();
-        val timers : ArrayList<Timer>? = medJsonReader.read()
-        print(timers);
-    }
+//    @Test
+//    fun jsonReaderTest() {
+//        val medJsonReader = MedJsonReader();
+//        val timers : ArrayList<Timer>? = medJsonReader.read()
+//    }
 
     @Test
-    fun jsonWriterTest() {
-        val medJsonReader = MedJsonReader();
-        val timers : ArrayList<Timer>? = medJsonReader.read()
-        medJsonReader.write(timers);
+    fun jsonReadWriteTest() {
+        val jsonTextBefore = File("src/main/java/com/lovelace_scd/timed/io/sample.json").readText();
+
+        // Not a robust test (pretty bad actually), but good enough for what we're doing.
+        for(i in 0..5) {
+            val medJsonReader = MedJsonReader();
+            val timers: ArrayList<Timer>? = medJsonReader.read()
+            medJsonReader.write(timers);
+        }
+
+        val jsonTextAfter = File("src/main/java/com/lovelace_scd/timed/io/sample.json").readText();
+        assertEquals("Compare json string before and after reads/writes.", jsonTextBefore, jsonTextAfter );
     }
 }
