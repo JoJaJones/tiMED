2d062fef7401210fec169f189dcec6fd0e470d0d
Josh Jones <jonesjo7@oregonstate.edu>
2020-09-11 22:07:21 -0700

    Data extraction from the form completed

--- app/src/main/java/com/lovelace_scd/timed/Controller/AddActivity.kt
+++ app/src/main/java/com/lovelace_scd/timed/Controller/AddActivity.kt
@@ -4,8 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
 import com.lovelace_scd.timed.R
+import java.sql.Time
 
 class AddActivity: AppCompatActivity() {
     private val TAG = "ActivityLifeCycle(AA): "
@@ -21,6 +26,54 @@ class AddActivity: AppCompatActivity() {
     }
 
     fun saveMed(view: View){
+        var tempString = ""
+        var doseFreq = -1
+        var dosePeriod = -1
+        var doseSize = -1.0
+        var refillRemaining = -1
+        var refillSize = -1.0
+        var name = findViewById<TextInputEditText>(R.id.medNameInput).text.toString()
+
+        tempString = findViewById<EditText>(R.id.doseFreqInput).text.toString()
+        if (tempString.length > 0) {
+            doseFreq = tempString.toDouble().toInt()
+        }
+
+        tempString = findViewById<EditText>(R.id.dosePeriodInput).text.toString()
+        if (tempString.length > 0) {
+            dosePeriod = tempString.toDouble().toInt()
+        }
+
+        tempString = findViewById<EditText>(R.id.doseSizeInput).text.toString()
+        if (tempString.length > 0) {
+            doseSize = tempString.toDouble()
+        }
+        var doseTimeStr = findViewById<EditText>(R.id.doseTimeInput).text.toString()
+        var doseDateStr = findViewById<EditText>(R.id.doseDateInput).text.toString()
+
+        tempString  = findViewById<EditText>(R.id.remRefillInput).text.toString()
+        if (tempString.length > 0) {
+            refillRemaining = tempString.toDouble().toInt()
+        }
+
+        tempString = findViewById<EditText>(R.id.refillSizeInput).text.toString()
+        if (tempString.length > 0) {
+            refillSize = tempString.toDouble()
+        }
+
+        var withFood = findViewById<Switch>(R.id.foodSwitch).isChecked
+        var doseUnit: String = "N/A"
+
+        if(findViewById<RadioButton>(R.id.pillBtn).isChecked) {
+            doseUnit = "pill"
+        } else if (findViewById<RadioButton>(R.id.mlBtn).isChecked) {
+            doseUnit = "mL"
+        } else if (findViewById<RadioButton>(R.id.ccBtn).isChecked) {
+            doseUnit = "CC"
+        }
+        Log.d("Form Test: ", "${name} ${doseFreq} ${dosePeriod} ${doseSize}\n" +
+                "${doseTimeStr} ${doseDateStr} ${refillRemaining} ${refillSize}\n" +
+                "${withFood} $doseUnit")
         // TODO: med saving/validating code here
         finish()
     }
--- app/src/main/java/com/lovelace_scd/timed/services/TestTimerObjects.kt
+++ app/src/main/java/com/lovelace_scd/timed/services/TestTimerObjects.kt
@@ -8,9 +8,9 @@ object TestTimerObjects {
             TestTimer("Zyrtec", 2, listOf("24:00:00", "12:00:00", "5:00")),
             TestTimer("Penicillin", 0, listOf("12:00:00", "8:00:00", "30")),
             TestTimer("Adderall", 0, listOf("11:00:00", "7:00:00", "31")),
-            TestTimer("Aspirin", 5, listOf("4:00:00", "1:23:45", "2:02:02")),
-            TestTimer("Zyrtec", 2, listOf("24:00:00", "12:00:00", "5:00")),
-            TestTimer("Penicillin", 0, listOf("12:00:00", "8:00:00", "30")),
-            TestTimer("Adderall", 0, listOf("11:00:00", "7:00:00", "31")),
+            TestTimer("Prozac", 5, listOf("4:00:00", "1:23:45", "2:02:02")),
+            TestTimer("Zoloft", 2, listOf("24:00:00", "12:00:00", "5:00")),
+            TestTimer("Xanax", 0, listOf("12:00:00", "8:00:00", "30")),
+            TestTimer("Vitamin", 0, listOf("11:00:00", "7:00:00", "31")),
     )
 }
