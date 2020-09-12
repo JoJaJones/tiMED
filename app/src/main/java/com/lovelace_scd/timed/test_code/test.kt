923ccccb45230fe9203f4f4dea4150b996829fec
Josh Jones <jonesjo7@oregonstate.edu>
2020-09-11 17:41:06 -0700

    merge issue corrected (hopefully)

---
+++ app/src/main/java/com/lovelace_scd/timed/test_code/test.kt
@@ -0,0 +1,10 @@
+package com.lovelace_scd.timed.test_code
+class Test(){
+    var array = ArrayList<Int>()
+
+    fun main(){
+        for (i in 0..10) array.add(i)
+
+        array.forEach() {e -> print(e)}
+    }
+}
