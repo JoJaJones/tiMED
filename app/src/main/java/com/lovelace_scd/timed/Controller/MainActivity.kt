8566d680fbd50362f8e11fb95e555e3067559dbe
Alex <wilsoal9@oregonstate.edu>
2020-09-12 17:14:41 -0600

    Notification set up

--- app/src/main/java/com/lovelace_scd/timed/Controller/MainActivity.kt
+++ app/src/main/java/com/lovelace_scd/timed/Controller/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.lovelace_scd.timed.Controller
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
 import android.content.res.Configuration
+import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
@@ -9,6 +15,8 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.lovelace_scd.timed.Adaptors.TimerAdaptor
@@ -21,10 +29,12 @@ class MainActivity : AppCompatActivity() {
     private val TAG = "ActivityLifeCycle(MA): "
     private val timers = TestTimerObjects.testTimers
     private lateinit var adapter: TimerAdaptor
+    private val CHANNEL_ID = "medication_alerts"
+    private val notificationId = 101
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        createNotificationChannel()
         setContentView(R.layout.activity_main)
-
         if (timers.size > 0) {
             adapter = TimerAdaptor(this, timers)
             val timerListView = findViewById<RecyclerView>(R.id.timerListView)
@@ -67,5 +77,38 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         Log.d(TAG, "onDestroy caled")
         super.onDestroy()
+    }
+
+    fun createNotificationChannel(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val name = "Medication Alerts"
+            val descriptionText = "Notifications when medications are ready to take"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID,name,importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    fun sendNotification() {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+        val notificationIcon = BitmapFactory.decodeResource(applicationContext.resources, R.drawable.pill_clipart)
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("Medication Ready")
+                .setContentText("You have a medication to take")
+                .setLargeIcon(notificationIcon)
+                .setContentIntent(pendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        with(NotificationManagerCompat.from(this)) {
+            notify(notificationId, builder.build())
+        }
     }
 }
