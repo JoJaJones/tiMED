33228e80c9da5bfa221a01bc79ab5530a4e2cc9a
Josh Jones <jonesjo7@oregonstate.edu>
2020-09-12 21:23:49 -0700

    I forgot to commit... So here you go.

--- app/src/main/java/com/lovelace_scd/timed/model/TimerData.kt
+++ app/src/main/java/com/lovelace_scd/timed/model/TimerData.kt
@@ -10,8 +10,7 @@ import java.io.File
 @RequiresApi(Build.VERSION_CODES.O)
 class TimerData {
     var timerData : ArrayList<Timer>;
-//    val fileName = "sample.json";
-        val fileName = "timers.json"
+    val fileName = "timers.json"
     private val jsonReader = MedJsonReader();
 
     constructor () {
